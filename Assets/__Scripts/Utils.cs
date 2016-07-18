4ff130a249655a72e20d824b43f7be00a642c866
christianjnegri <christianjnegri@gmail.com>
2016-07-18 15:08:35 -0400

    Bartok Layout
    Bartok layout set up

--- Assets/__Scripts/Utils.cs
+++ Assets/__Scripts/Utils.cs
@@ -260,6 +260,31 @@ public class Utils : MonoBehaviour {
 	// While most Bézier curves are 3 or 4 points, it is possible to have
 	//   any number of points using this recursive function
 	// This uses the Utils.Lerp function because it needs to allow extrapolation
+	static public Quaternion Bezier( float u, List<Quaternion> vList ) {
+		// If there is only one element in vList, return it
+		if (vList.Count == 1) {
+			return( vList[0] );
+		}
+		// Otherwise, create vListR, which is all but the 0th element of vList
+		// e.g. if vList = [0,1,2,3,4] then vListR = [1,2,3,4]
+		List<Quaternion> vListR =  vList.GetRange(1, vList.Count-1);
+		// And create vListL, which is all but the last element of vList
+		// e.g. if vList = [0,1,2,3,4] then vListL = [0,1,2,3]
+		List<Quaternion> vListL = vList.GetRange(0, vList.Count-1);
+		// The result is the Slerp of these two shorter Lists
+		// It's possible that Quaternion.Slerp may clamp u to [0..1] :(
+		Quaternion res = Quaternion.Slerp(Bezier(u,vListL), Bezier(u,vListR), u);
+		return( res );
+	}
+	
+	// This version allows an Array or a series of Quaternions as input
+	static public Quaternion Bezier( float u, params Quaternion[] vecs ) {
+		return( Bezier( u, new List<Quaternion>(vecs) ) );
+	}
+	
+	
+
+
 	static public Vector3 Bezier( float u, List<Vector3> vList ) {
 		// If there is only one element in vList, return it
 		if (vList.Count == 1) {
